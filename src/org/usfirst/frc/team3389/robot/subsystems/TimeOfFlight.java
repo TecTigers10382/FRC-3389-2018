af4cfe9cafc4be47b0a4febabc2ecdc22af8d1dd
Boo1098 <nathansd1098@gmail.com>
2018-01-13 19:08:58 -0500

    fixed code in ToF

--- src/org/usfirst/frc/team3389/robot/subsystems/TimeOfFlight.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/TimeOfFlight.java
@@ -9,15 +9,9 @@ package org.usfirst.frc.team3389.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * An example subsystem.  You can replace me with your own Subsystem.
- */
 public class TimeOfFlight extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
 
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+		
 	}
 }

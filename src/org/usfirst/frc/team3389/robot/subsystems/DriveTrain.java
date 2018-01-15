c8a84090c114a2679e7c96f41e06ccb36b847239
Nathan SD <Boo1098@users.noreply.github.com>
2018-01-14 20:23:27 -0500

    Drive train is now actually a subsystem

--- src/org/usfirst/frc/team3389/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/DriveTrain.java
@@ -1,5 +1,13 @@
 package org.usfirst.frc.team3389.robot.subsystems;
 
-public class DriveTrain {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

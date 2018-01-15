788a117e8d34c28be4f8bd023ab9469fe32649ac
Nathan SD <Boo1098@users.noreply.github.com>
2018-01-14 20:04:28 -0500

    Revert "Fixed some imports"
    This reverts commit abefc39b0dbaabace5d8f0d2315a9c7e0084cba2.

--- src/org/usfirst/frc/team3389/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team3389/robot/commands/ExampleCommand.java
@@ -8,6 +8,7 @@
 package org.usfirst.frc.team3389.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3389.robot.Robot;
 
 /**
  * An example command. You can replace me with your own command.
@@ -15,7 +16,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ExampleCommand extends Command {
 	public ExampleCommand() {
 		// Use requires() here to declare subsystem dependencies
-		// requires(Robot.kExampleSubsystem);
+		//requires(Robot.kExampleSubsystem);
 	}
 
 	// Called just before this Command runs the first time

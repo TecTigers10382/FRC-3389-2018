b8691286b40431436676b215e4423f081e87bf23
Pritika-Papali <papalipritika@gmail.com>
2018-01-21 14:15:06 -0500

    Added Logger Debugging

--- src/org/usfirst/frc/team3389/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/DriveTrain.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team3389.robot.subsystems;
 
+import org.usfirst.frc.team3389.robot.Robot;
 import org.usfirst.frc.team3389.robot.RobotMap;
 import org.usfirst.frc.team3389.robot.commands.Drive;
+import org.usfirst.frc.team3389.robot.utils.Logger;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -10,36 +12,57 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 @SuppressWarnings("deprecation")
 public class DriveTrain extends Subsystem {
+	
+	
 	TalonSRX leftFront;
 	TalonSRX leftBack;
 	TalonSRX rightFront;
 	TalonSRX rightBack;	
 	
 	public DriveTrain() {
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
+		
 		leftFront = new TalonSRX(RobotMap.DRIVE_LEFTFRONT);
 		leftBack = new TalonSRX(RobotMap.DRIVE_LEFTBACK);
 		rightFront = new TalonSRX(RobotMap.DRIVE_RIGHTFRONT);
 		rightBack = new TalonSRX(RobotMap.DRIVE_RIGHTBACK);	
+		
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	@Override
 	protected void initDefaultCommand() {
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
 		// TODO Auto-generated method stub
 		setDefaultCommand(new Drive());
+		
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
 	private void rawDrive(double leftPower, double rightPower){
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
+		
 		leftFront.set(ControlMode.Current, leftPower);
 		leftBack.set(ControlMode.Current, leftPower);
 		rightFront.set(ControlMode.Current, rightPower);
 		rightBack.set(ControlMode.Current, rightPower);
+		
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
 	public void tankDrive(double left,double right){
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
+		
 		rawDrive(left,right);
+		
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
 	public void Stop(){
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
+		
 		rawDrive(0,0);
+		
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
 }

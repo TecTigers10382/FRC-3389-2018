833b3d954a18a69890a7de78964bc4e9fee3a45f
booolean <eraycarey@gmail.com>
2018-01-21 13:40:17 -0500

    Fixed Test.java

--- src/org/usfirst/frc/team3389/robot/commands/Test.java
+++ src/org/usfirst/frc/team3389/robot/commands/Test.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team3389.robot.commands;
 
-import org.usfirst.frc.team3389.robot.subsystems.Logger;
-
 public class Test{
-	Logger.log();
+
 	//This is a test of eclipse git integration
 }

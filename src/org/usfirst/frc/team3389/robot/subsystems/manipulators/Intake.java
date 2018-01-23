6f7c4a0b3c8add5f2e47f2f877d5843fed8221ef
Pritika-Papali <papalipritika@gmail.com>
2018-01-23 17:07:41 -0500

    Intake now intakes

--- src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Intake.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Intake.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3389.robot.subsystems.manipulators;
 
 import org.usfirst.frc.team3389.robot.RobotMap;
+import org.usfirst.frc.team3389.robot.commands.Drive;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,22 +9,23 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Intake extends Subsystem {
 	Spark intakeLeft;
 	Spark intakeRight;
-	
+
 	public Intake() {
 		intakeLeft = new Spark(RobotMap.INTAKE_LEFT);
 		intakeRight = new Spark(RobotMap.INTAKE_RIGHT);
-		
+
 	}
-	
-	public void driveBoth (double power) {
+
+	public void driveBoth(double power) {
 		intakeLeft.set(power);
 		intakeRight.set(power);
 	}
-	
+
 	public void stop() {
 		driveBoth(0);
 	}
+
 	protected void initDefaultCommand() {
-		
+		setDefaultCommand(new Drive());
 	}
 }

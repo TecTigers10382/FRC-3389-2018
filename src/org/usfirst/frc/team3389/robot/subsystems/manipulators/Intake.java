f47feecaedac5cad844c34d5b343ffbf059f8cf5
Boo1098 <nathansd1098@gmail.com>
2018-01-23 18:11:52 -0500

    Update Javadoc

--- src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Intake.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Intake.java
@@ -12,39 +12,54 @@ public class Intake extends Subsystem {
 	Spark intakeLeft;
 	Spark intakeRight;
 
+	/**
+	 * Constructor. Initializes Sparks.
+	 */
 	public Intake() {
 		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
 
 		intakeLeft = new Spark(RobotMap.INTAKE_LEFT);
 		intakeRight = new Spark(RobotMap.INTAKE_RIGHT);
-		
+
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 
 	}
 
+	/**
+	 * Intakes cubes by spinning motors at power
+	 * 
+	 * @param power
+	 *            power of motor from -1.0 to 1.0
+	 */
 	public void driveBoth(double power) {
-		Robot.robotLogger.log(Logger.DEBUG, this, "enter:\t"+power);
-		
+		Robot.robotLogger.log(Logger.DEBUG, this, "enter:\t" + power);
+
 		intakeLeft.set(-power);
 		intakeRight.set(power);
-		
+
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 
+	/**
+	 * Stops all motors on intake.
+	 */
 	public void stop() {
 		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
 
 		driveBoth(0);
-		
+
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 
 	}
 
+	/**
+	 * Intializes default command of IntakeStick.
+	 */
 	protected void initDefaultCommand() {
 		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
 
 		setDefaultCommand(new IntakeStick());
-		
+
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 
 	}

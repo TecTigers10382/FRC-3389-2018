0ed3342058d9b14864521e43d9ca45f1487734c3
booolean <eraycarey@gmail.com>
2018-02-12 16:58:26 -0500

    Added some debugging stuff to the lifter

--- src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Lifter.java
+++ src/org/usfirst/frc/team3389/robot/subsystems/manipulators/Lifter.java
@@ -7,21 +7,29 @@ import org.usfirst.frc.team3389.robot.subsystems.ioDevices.QuadEncoder;
 import org.usfirst.frc.team3389.robot.utils.Logger;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.Faults;
+import com.ctre.phoenix.motorcontrol.StickyFaults;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lifter extends Subsystem{
 	TalonSRX lift;
+	StickyFaults liftSFaults = new StickyFaults();
+	Faults LiftFaults = new Faults();
+	Encoder enc = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+	
+
 	private double height;
 	private double radius;
-	public static final QuadEncoder enc = new QuadEncoder();
-		
+	
 	public Lifter() {
 		Robot.robotLogger.log(Logger.DEBUG, this, "enter");
 		
 		lift = new TalonSRX(RobotMap.LIFT);
-		
+		lift.setNeutralMode(com.ctre.phoenix.motorcontrol.NeutralMode.Brake);
+		Debug();
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
@@ -49,8 +57,9 @@ public class Lifter extends Subsystem{
 		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
 	}
 	
-	public void getHeight() {
-		//height=radius*(enc.getVal()/360);
+	public double getHeight() {
+		height=radius*(enc.get()/360);
+		return height;
 	}
 	public void gotoHeight(int inches) {
 		getHeight();
@@ -67,5 +76,33 @@ public class Lifter extends Subsystem{
 		
 		
 		
+	}
+	private void Debug() {
+		Robot.robotLogger.log(Logger.DEBUG, this, "TALON DEBUG\n==================================");
+		Robot.robotLogger.log(Logger.DEBUG, this, "Output Current");
+		Robot.robotLogger.log(Logger.DEBUG, this, "lift : " + lift.getOutputCurrent());
+
+		Robot.robotLogger.log(Logger.DEBUG, this, "Output Voltage");
+		Robot.robotLogger.log(Logger.DEBUG, this, "lift : " + lift.getMotorOutputVoltage());
+
+		Robot.robotLogger.log(Logger.DEBUG, this, "Bus Voltage");
+		Robot.robotLogger.log(Logger.DEBUG, this, "lift : " + lift.getBusVoltage());
+		
+
+		Robot.robotLogger.log(Logger.DEBUG, this, "Output Percent");
+		Robot.robotLogger.log(Logger.DEBUG, this, "lift: " + lift.getMotorOutputPercent());
+		Robot.robotLogger.log(Logger.DEBUG, this, "exit");
+
+		// Talon Faults
+		Robot.robotLogger.log(Logger.DEBUG, this, lift.getFaults(LiftFaults).toString());
+		
+		// Talon Stick Faults
+		Robot.robotLogger.log(Logger.DEBUG, this, lift.getStickyFaults(liftSFaults).toString());
+
+		clearStickyFaults();
+	}
+
+	private void clearStickyFaults() {
+		lift.clearStickyFaults(0);
 	}
 }

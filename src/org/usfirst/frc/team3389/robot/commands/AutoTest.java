9671555894beb1f5a6aea16769da88c6e6120d86
TecTiger <TecTiger@TecTiger-PC.wcboe.local>
2018-02-18 17:14:22 -0500

    Minor Change to PID drive

--- src/org/usfirst/frc/team3389/robot/commands/AutoTest.java
+++ src/org/usfirst/frc/team3389/robot/commands/AutoTest.java
@@ -25,7 +25,7 @@ public class AutoTest extends Command {
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		DriveTrain.drivePosition(100,100);
+		DriveTrain.drivePosition(-1,1);
 	}
 
 	// Called repeatedly when this Command is scheduled to run

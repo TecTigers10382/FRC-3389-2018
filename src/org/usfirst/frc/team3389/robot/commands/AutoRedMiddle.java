7b85ab552d65064a009194c0c34e7664246c884a
bradanlane <glen@bradanlane.com>
2018-03-07 13:14:39 -0500

    created skeleton command groups for the various autonomous positions

--- src/org/usfirst/frc/team3389/robot/commands/AutoRedMiddle.java
+++ src/org/usfirst/frc/team3389/robot/commands/AutoRedMiddle.java
@@ -1,47 +1,24 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/*
+* Copyright (c) 2017-2018 FRC TEAM 3389. All Rights Reserved.
+* Open Source Software - may be modified and shared by FRC teams. The code
+* must be accompanied by the FIRST BSD license file in the root directory of
+* the project.
+*/
+
 
 package org.usfirst.frc.team3389.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- * An example command. You can replace me with your own command.
- */
-public class AutoRedMiddle extends Command {
+public class AutoRedMiddle extends CommandGroup {
+	// private properties here
+	
 	public AutoRedMiddle() {
-		// Use requires() here to declare subsystem dependencies
-		//requires(Robot.kExampleSubsystem);
-	}
-
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {
-	}
-
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	protected boolean isFinished() {
-		return false;
-	}
-
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-	}
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {
+		// add any combination of commands in sequential or parallel execution
+		// for example:
+		//   addSequential(new CommandA());
+		//   addParallel(new CommandB());
+		//   addParallel(new CommandC());
+		//   addSequential(new CommandD());
 	}
 }
--- src/org/usfirst/frc/team3389/robot/commands/TestCommandGroup.java
+++ src/org/usfirst/frc/team3389/robot/commands/TestCommandGroup.java
@@ -1,3 +1,11 @@
+/*
+* Copyright (c) 2017-2018 FRC TEAM 3389. All Rights Reserved.
+* Open Source Software - may be modified and shared by FRC teams. The code
+* must be accompanied by the FIRST BSD license file in the root directory of
+* the project.
+*/
+
+
 package org.usfirst.frc.team3389.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;

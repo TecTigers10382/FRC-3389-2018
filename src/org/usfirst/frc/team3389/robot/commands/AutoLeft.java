790b48a0f4469817e32afbe80b99a71493e96c3a
Pritika-Papali <papalipritika@gmail.com>
2018-03-13 17:50:53 -0400

    Edited the AutoLeft command

--- src/org/usfirst/frc/team3389/robot/commands/AutoLeft.java
+++ src/org/usfirst/frc/team3389/robot/commands/AutoLeft.java
@@ -14,6 +14,24 @@ public class AutoLeft extends CommandGroup {
 	// private properties here
 	
 	public AutoLeft(int gameData) {
+		
+		if(gameData == 0 || gameData == 2) {
+			addParallel(new LiftAuto(3.5));
+			addSequential(new DriveDistance(324.6));
+			addSequential(new DriveTurn(.5, 90));
+			addSequential(new IntakeAuto(-1, 2));
+		}
+		else if(gameData == 1 || gameData == 3) {
+			addSequential(new DriveDistance(223.2));
+			addSequential(new DriveTurn(.5, 90));
+			addParallel(new LiftAuto(3.5));
+			addSequential(new DriveDistance(207.1));
+			addSequential(new DriveTurn(.5, -90));
+			addSequential(new DriveDistance(41.6));
+		}
+		
+		
+		
 		// add any combination of commands in sequential or parallel execution
 		// for example:
 		//   addSequential(new CommandA());

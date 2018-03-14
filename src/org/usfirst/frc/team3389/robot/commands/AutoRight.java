1d39347f461ef826cef7a671a58267c364f575fc
bradanlane <bradanlane@users.noreply.github.com>
2018-03-14 07:06:28 -0400

    added TODO comments

--- src/org/usfirst/frc/team3389/robot/commands/AutoRight.java
+++ src/org/usfirst/frc/team3389/robot/commands/AutoRight.java
@@ -15,25 +15,21 @@ public class AutoRight extends CommandGroup {
 	
 	public AutoRight(int gameData) {
 		if(gameData == 1 || gameData == 3) {
+			// TODO add description of this autonomous maneuver
 			addParallel(new LiftAuto(3.5));
 			addSequential(new DriveDistance(303.8));
 			addSequential(new DriveTurn(.5, -90));
-			addSequential(new IntakeAuto(-1, 2));
+			addSequential(new IntakeAuto(-1, 2)); // TODO this could move outside if/then
 		}
 		else if(gameData == 0 || gameData == 2) {
+			// TODO add description of this autonomous maneuver
 			addSequential(new DriveDistance(223.2));
 			addSequential(new DriveTurn(.5, -90));
 			addParallel(new LiftAuto(3.5));
 			addSequential(new DriveDistance(207.1));
 			addSequential(new DriveTurn(.5, 90));
 			addSequential(new DriveDistance(41.6));
-			addSequential(new IntakeAuto(-1, 2));
+			addSequential(new IntakeAuto(-1, 2)); // TODO this could move outside if/then
 		}
-		// add any combination of commands in sequential or parallel execution
-		// for example:
-		//   addSequential(new CommandA());
-		//   addParallel(new CommandB());
-		//   addParallel(new CommandC());
-		//   addSequential(new CommandD());
 	}
 }

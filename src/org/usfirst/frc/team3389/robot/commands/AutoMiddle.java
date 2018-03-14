a3c00ae060abbf34bd80a2312d7ad92c605cd833
bradanlane <bradanlane@users.noreply.github.com>
2018-03-14 07:02:18 -0400

    Added TODO comments

--- src/org/usfirst/frc/team3389/robot/commands/AutoMiddle.java
+++ src/org/usfirst/frc/team3389/robot/commands/AutoMiddle.java
@@ -15,28 +15,24 @@ public class AutoMiddle extends CommandGroup {
 	
 	public AutoMiddle(int gameData) {
 		if(gameData == 0 || gameData == 1) {
+			// TODO add description of this autonomous maneuver
 			addSequential(new DriveDistance(46.5));
 			addSequential(new DriveTurn(.5, -45));
 			addParallel(new LiftAuto(3));
 			addSequential(new DriveDistance(85.4));
 			addSequential(new DriveTurn(.5, 45));
 			addSequential(new DriveDistance(19.9));
-			addSequential(new IntakeAuto(-1, 2));	
+			addSequential(new IntakeAuto(-1, 2)); // TODO this could move outside if/then
 		}
 		else if(gameData == 2 || gameData == 3) {
+			// TODO add description of this autonomous maneuver
 			addSequential(new DriveDistance(46.5));
 			addSequential(new DriveTurn(.5, 45));
 			addParallel(new LiftAuto(3));
 			addSequential(new DriveDistance(67.3));
 			addSequential(new DriveTurn(.5, -45));
 			addSequential(new DriveDistance(39));
-			addSequential(new IntakeAuto(-1, 2));
+			addSequential(new IntakeAuto(-1, 2)); // TODO this could move outside if/then
 		}
-		// add any combination of commands in sequential or parallel execution
-		// for example:
-		//   addSequential(new CommandA());
-		//   addParallel(new CommandB());
-		//   addParallel(new CommandC());
-		//   addSequential(new CommandD());
 	}
 }
